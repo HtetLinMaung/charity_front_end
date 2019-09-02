7e6f795512eac70a5fd8318815fd612e0bb4a1c5
Jst <jsthtet96@gmail.com>
2019-09-02 22:14:01 +0630

    add router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router'
 import App from './App.vue';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter)
 
 new Vue({
   store,
