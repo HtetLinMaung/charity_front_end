05f09c2c528ec2cb130fef053aad1a5662f4254a
waiyanyenaing <waiayanyenaing@.com>
2019-09-03 22:19:45 +0630

    update

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,34 +1,355 @@
 <template>
   <div class="messages">
-    <h1>zzz</h1>
-
+    
     <v-layout row>
       <v-col class="message_card" align="end" justify="end">
         <v-card max-width="344" class="ml-auto mt-auto">
-          <v-card-title class="m_card_title"><v-icon>mdi-message</v-icon>&nbsp;I'm a title</v-card-title>
-          <v-card-text>I'm card text</v-card-text>
-          <v-textarea></v-textarea>
-          <v-card-actions>
-            <v-btn text>Send</v-btn>
-          </v-card-actions>
+          <v-card-title class="m_card_title">
+            <v-icon>mdi-message</v-icon>&nbsp;I'm a title
+          </v-card-title>
+          <v-card-text>
+            <p :class="{ send: isSend }">hola</p>
+            <p>hello</p>
+            <p :class="{ send: isSend }">hello</p>
+            <p :class="{ send: isSend }">hey</p>
+            <p>hi</p>
+            <p>ho</p>
+            <p :class="{ send: isSend }">text1</p>
+            <p>text1</p>
+            <p>text1</p>
+            <p :class="{ send: isSend }">text1</p>
+            <p>text1</p>
+            <p :class="{ send: isSend }">text1</p>
+          </v-card-text>
+          <v-textarea filled auto-grow rows="1" row-height="30" append-icon="mdi-emoticon-poop" label="Send Message"></v-textarea>
         </v-card>
       </v-col>
     </v-layout>
+    <!-- xaxa -->
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+      <h1>zzz</h1><br>
+    <!-- xaxa -->
+
   </div>
 </template>
 
 <script>
 export default {
-  data: () => ({}),
+  data: () => ({
+    isSend: true,
+  })
 };
 </script>
 <style>
 .message_card {
-  position: absolute;
-  padding: 25% 15% 0 78%;
+  position: fixed;
+  /* padding: 25% 15% 0 78%;
+  padding: 20% 0% 0 40%; */
+  padding-top: 12rem;
 }
 .message_card .v-card {
   min-width: 330px;
+  margin-right: 30rem;
 }
 .message_card .v-card__title {
   background-color: lightseagreen;
@@ -36,5 +357,22 @@ export default {
   height: 3rem;
   line-height: 0rem;
   padding: 10px 16px 9px;
+}
+.message_card .theme--light.v-card > .v-card__text {
+  height: 250px;
+  overflow-y: scroll;
+}
+.m-textarea {
+  width: 15px;
+}
+.message_card .v-input__slot {
+  margin-bottom: 0px;
+}
+.message_card .v-input__slot{
+  margin-bottom: -21px;
+}
+.message_card .send{
+  margin-left: auto;
+  text-align: left;
 }
 </style>
