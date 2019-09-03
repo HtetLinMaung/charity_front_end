e52e56116ea6bf8e1c926c90ef626c793a219ab3
Kyi Sin Thant <kyi.sin.thant@fusionsol.com>
2019-09-03 13:12:30 +0630

    edit

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -25,66 +25,19 @@
         <v-btn icon>
           <v-icon>mdi-heart</v-icon>
         </v-btn>
-        <v-btn text>Click</v-btn>
-        <v-btn outlined>Click</v-btn>
-      </v-card-actions>
-    </v-card>
-    <v-card
-      class="mx-auto"
-      :flat="flat"
-      :loading="loading"
-      :outlined="outlined"
-      :elevation="elevation"
-      :raised="raised"
-      :width="width"
-      :height="height"
-    >
-      <v-img
-        v-if="media"
-        class="white--text"
-        height="500px"
-        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-      >
-        <v-card-title class="align-end fill-height">I'm a title</v-card-title>
-      </v-img>
-      <v-card-title v-else>I'm a title</v-card-title>
-
-      <v-card-text>I'm card text</v-card-text>
-      <v-card-actions v-if="actions">
         <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
+          <v-icon>mdi-comment-processing-outline</v-icon>
         </v-btn>
-        <v-btn text>Click</v-btn>
-        <v-btn outlined>Click</v-btn>
-      </v-card-actions>
-    </v-card>
-    <v-card
-      class="mx-auto"
-      :flat="flat"
-      :loading="loading"
-      :outlined="outlined"
-      :elevation="elevation"
-      :raised="raised"
-      :width="width"
-      :height="height"
-    >
-      <v-img
-        v-if="media"
-        class="white--text"
-        height="500px"
-        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-      >
-        <v-card-title class="align-end fill-height">I'm a title</v-card-title>
-      </v-img>
-      <v-card-title v-else>I'm a title</v-card-title>
-
-      <v-card-text>I'm card text</v-card-text>
-      <v-card-actions v-if="actions">
         <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
+          <v-icon>mdi-tag-outline</v-icon>
         </v-btn>
-        <v-btn text>Click</v-btn>
-        <v-btn outlined>Click</v-btn>
+        <div class="flex-grow-1"></div>
+        <v-btn icon>
+          <v-icon>mdi-bookmark</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-share-variant</v-icon>
+        </v-btn>
       </v-card-actions>
     </v-card>
   </div>
