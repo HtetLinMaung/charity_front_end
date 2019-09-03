80767045270ba3d49ae3eb018bc535bf7556aa59
Jst <jsthtet96@gmail.com>
2019-09-03 11:14:40 +0630

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,26 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+    <nav-drawer />
+    <nav-bar />
 
     <v-content>
-     
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-
+import NavBar from "./components/NavBar";
+import NavDrawer from "./components/NavDrawer";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  
+    "nav-bar": NavBar,
+    "nav-drawer": NavDrawer
   },
   data: () => ({
     //
-  }),
+  })
 };
 </script>
