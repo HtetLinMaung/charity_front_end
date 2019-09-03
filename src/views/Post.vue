5cbe5923fbf24507e409fcc5bc5cce4239994ff3
Kyi Sin Thant <kyi.sin.thant@fusionsol.com>
2019-09-03 12:50:03 +0630

    edit

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,13 +1,113 @@
 <template>
-     <div class="post">
-    <h1>This is an Post page</h1>
+  <div class="post mt-5">
+    <v-card
+      class="mx-auto"
+      :flat="flat"
+      :loading="loading"
+      :outlined="outlined"
+      :elevation="elevation"
+      :raised="raised"
+      :width="width"
+      :height="height"
+    >
+      <v-img
+        v-if="media"
+        class="white--text"
+        height="500px"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+        <v-card-title class="align-end fill-height">I'm a title</v-card-title>
+      </v-img>
+      <v-card-title v-else>I'm a title</v-card-title>
+
+      <v-card-text>I'm card text</v-card-text>
+      <v-card-actions v-if="actions">
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <v-btn text>Click</v-btn>
+        <v-btn outlined>Click</v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card
+      class="mx-auto"
+      :flat="flat"
+      :loading="loading"
+      :outlined="outlined"
+      :elevation="elevation"
+      :raised="raised"
+      :width="width"
+      :height="height"
+    >
+      <v-img
+        v-if="media"
+        class="white--text"
+        height="500px"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+        <v-card-title class="align-end fill-height">I'm a title</v-card-title>
+      </v-img>
+      <v-card-title v-else>I'm a title</v-card-title>
+
+      <v-card-text>I'm card text</v-card-text>
+      <v-card-actions v-if="actions">
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <v-btn text>Click</v-btn>
+        <v-btn outlined>Click</v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card
+      class="mx-auto"
+      :flat="flat"
+      :loading="loading"
+      :outlined="outlined"
+      :elevation="elevation"
+      :raised="raised"
+      :width="width"
+      :height="height"
+    >
+      <v-img
+        v-if="media"
+        class="white--text"
+        height="500px"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+        <v-card-title class="align-end fill-height">I'm a title</v-card-title>
+      </v-img>
+      <v-card-title v-else>I'm a title</v-card-title>
+
+      <v-card-text>I'm card text</v-card-text>
+      <v-card-actions v-if="actions">
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <v-btn text>Click</v-btn>
+        <v-btn outlined>Click</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      flat: false,
+      media: true,
+      loading: false,
+      actions: true,
+      outlined: false,
+      elevation: undefined,
+      raised: false,
+      width: 650,
+      height: undefined,
+    };
+  },
+};
 </script>
-<style>
-
+<style scoped>
+.v-input__slider {
+  width: 100%;
+}
 </style>
