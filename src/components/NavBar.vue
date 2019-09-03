f5373d01b06b6ffe9baf894412002a043f5992d6
Jst <jsthtet96@gmail.com>
2019-09-03 16:57:41 +0630

    add icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,26 +7,18 @@
     <v-spacer></v-spacer>
 
     <v-btn icon>
-      <v-icon>mdi-heart</v-icon>
+      <v-icon>mdi-home-outline</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-account-outline</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-heart-outline</v-icon>
     </v-btn>
 
     <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
+      <v-icon>mdi-telegram</v-icon>
     </v-btn>
-
-    <v-menu left bottom>
-      <template v-slot:activator="{ on }">
-        <v-btn icon v-on="on">
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-
-      <v-list>
-        <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-          <v-list-item-title>Option {{ n }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
   </v-app-bar>
 </template>
 
