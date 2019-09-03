856ba7de10a1a38bb3e7eb73a7c4fc717beb087a
waiyanyenaing <wai.yan.ye.naing@fusionsol.com>
2019-09-03 17:12:55 +0630

    updated

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,12 +1,39 @@
 <template>
-     <div class="messages">
-    <h1>This is an Messages page</h1>
+  <div class="messages">
+    <h1>zzz</h1>
+
+    <v-layout row>
+      <v-col class="message_card" align="end" justify="end">
+        <v-card max-width="344" class="ml-auto mt-auto">
+          <v-card-title class="m_card_title"><v-icon>mdi-message</v-icon>&nbsp;I'm a title</v-card-title>
+          <v-card-text>I'm card text</v-card-text>
+          <v-card-actions>
+            <v-btn text>Click</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-layout>
   </div>
 </template>
 
 <script>
-
+export default {
+  data: () => ({})
+};
 </script>
 <style>
-
+.message_card {
+  position: absolute;
+  padding: 25% 15% 0 78%;
+}
+.message_card .v-card {
+  min-width: 330px;
+}
+.message_card .v-card__title {
+  background-color: lightseagreen;
+  font-size: 1rem;
+  height: 3rem;
+  line-height: 0rem;
+  padding: 10px 16px 9px;
+}
 </style>
