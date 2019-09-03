9bd86d5fe78fbb79da617c47b9a37d6299c1e6ab
Kyi Sin Thant <kyi.sin.thant@fusionsol.com>
2019-09-03 17:09:00 +0630

    .

--- src/router.js
+++ src/router.js
@@ -9,21 +9,14 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [{
-<<<<<<< HEAD
     path: '/',
-    name: 'home',
-    component: Home,
-  }],
-=======
-      path: '/',
-      name: 'post',
-      component: Post,
-    },
-    {
-      path: "/messages",
-      name: "messages",
-      component: () => import(`@/views/Messages.vue`)
-    }
+    name: 'post',
+    component: Post,
+  },
+  {
+    path: '/messages',
+    name: 'messages',
+    component: () => import('@/views/Messages.vue'),
+  },
   ],
->>>>>>> 753d72af3e512cb14111e4c6f9bfce1782d88b11
 });
