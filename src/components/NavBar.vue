671e164ed28124d5d1dfcb0630a4b914c1d1fc63
htet_lin_maung <htet.lin.maung.@fusionsol.com>
2019-09-04 14:52:31 +0630

    add hamburger

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,32 +1,38 @@
 <template>
-  <v-app-bar app color dense class="social-toolbar">
-    <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+<v-app-bar app color dense class="social-toolbar">
+  <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
 
-    <v-toolbar-title v-if="!$vuetify.breakpoint.xs" class="logo-text" @click="redirect('/')">Hlu Mal</v-toolbar-title>
+  <v-btn icon v-if="$vuetify.breakpoint.xs">
+    <v-icon>mdi-view-headline</v-icon>
+  </v-btn>
 
-    <v-spacer v-if="!$vuetify.breakpoint.xs"></v-spacer>
+  <v-toolbar-title v-if="!$vuetify.breakpoint.xs" class="logo-text" @click="redirect('/')">Hluu Mal</v-toolbar-title>
 
-
-      <v-btn icon @click="redirect('/')">
-        <v-icon>mdi-home-outline</v-icon>
-      </v-btn>
-      <v-btn icon @click="redirect('/profile')">
-        <v-icon>mdi-account-outline</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-heart-outline</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-telegram</v-icon>
-      </v-btn>
+  <v-spacer v-if="!$vuetify.breakpoint.xs"></v-spacer>
 
 
-  </v-app-bar>
+  <v-btn icon @click="redirect('/')">
+    <v-icon>mdi-home-outline</v-icon>
+  </v-btn>
+  <v-btn icon @click="redirect('/profile')">
+    <v-icon>mdi-account-outline</v-icon>
+  </v-btn>
+  <v-btn icon>
+    <v-icon>mdi-heart-outline</v-icon>
+  </v-btn>
+
+  <v-btn icon>
+    <v-icon>mdi-telegram</v-icon>
+  </v-btn>
+
+
+</v-app-bar>
 </template>
 
 <script>
-import { utils } from '../mixins/functions';
+import {
+  utils
+} from '../mixins/functions';
 
 export default {
   mixins: [utils],
@@ -36,30 +42,30 @@ export default {
 <style lang="scss">
 $main-color: linear-gradient(45deg, #1dc8cd 0%, #55fabe 100%);
 .logo-text {
-  font-family: "Lobster", cursive;
-  background: black;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  cursor: pointer;
+    font-family: "Lobster", cursive;
+    background: black;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    cursor: pointer;
 }
 .flex-container {
-  display: flex;
+    display: flex;
 }
 @media all and (max-width: 414px) {
-  .logo-text {
-    display: hidden;
-  }
-  .social-toolbar {
-    .v-toolbar__content {
-      margin: 0!important;
-      display: flex;
-      justify-content: space-around;
+    .logo-text {
+        display: hidden;
     }
-  }
+    .social-toolbar {
+        .v-toolbar__content {
+            margin: 0!important !important;
+            display: flex;
+            justify-content: space-around;
+        }
+    }
 }
 .social-toolbar {
-  .v-toolbar__content {
-    margin: 0 6rem;
-  }
+    .v-toolbar__content {
+        margin: 0 6rem;
+    }
 }
 </style>
