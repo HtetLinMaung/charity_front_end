f32a4251e05035c6d67b62cbdaa7eb61e73b0a5d
Kyi Sin Thant <kyi.sin.thant@fusionsol.com>
2019-09-04 13:28:46 +0630

    edit

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -10,6 +10,7 @@
       :width="width"
       :height="height"
       :elevation="elevation"
+      @click="post"
     >
       <v-layout row>
         <v-avatar class="mt-3 ml-5 mb-3" size="40">
@@ -22,11 +23,24 @@
         </v-btn>
       </v-layout>
     </v-card>
+    <v-row justify="center">
+      <v-dialog v-model="dialog" persistent max-width="500">
+        <v-card>
+
+          <v-card-actions>
+            <div class="flex-grow-1"></div>
+            <v-btn color="primary" text @click="dialog = false">Cancel</v-btn>
+            <v-btn color="primary" text @click="dialog = false">Post</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
   </div>
 </template>
 <script>
 export default {
   data: () => ({
+    dialog: false,
     elevation: 4,
     flat: false,
     media: true,
@@ -42,6 +56,12 @@ export default {
       // eslint-disable-next-line comma-dangle
     }
   }),
+  methods: {
+    post() {
+      const self = this;
+      self.dialog = true;
+    },
+  },
 };
 </script>
 <style scoped>
