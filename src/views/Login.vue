89c958491fb4e179c2efc7fa512765bb707ef2d7
htet_lin_maung <htet.lin.maung.@fusionsol.com>
2019-09-05 08:50:02 +0630

    artist design fix

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <span class="dot1"></span>
-    <span class="dot2"></span>
-    <span class="dot3"></span>
-    <span class="dot4"></span>
-    <span class="dot5"></span>
-    <span class="dot6"></span>
-  </div>
+<div>
+  <span class="dot1"></span>
+  <span class="dot2"></span>
+  <span class="dot3"></span>
+  <span class="dot4"></span>
+  <span class="dot5"></span>
+  <span class="dot6"></span>
+</div>
 </template>
 
 <script>
@@ -15,67 +15,81 @@ export default {};
 
 <style lang="scss">
 .dot1 {
-  height: 350px;
-  width: 350px;
-  background-color: #34daff;
-  border-radius: 50%;
-  display: inline-block;
-  position: fixed;
-  top: 30%;
-  left: 89%;
+    height: 350px;
+    width: 350px;
+    background-color: #34daff;
+    border-radius: 50%;
+    display: inline-block;
+    position: fixed;
+    top: 30%;
+    left: 89%;
 }
 .dot2 {
-  width: 100px;
-  height: 100px;
-  background: #feb04e;
-  border-radius: 50%;
-  display: inline-block;
-  position: fixed;
-  top: 50%;
-  left: 85%;
-  opacity: 0.7;
+    width: 100px;
+    height: 100px;
+    background: #feb04e;
+    border-radius: 50%;
+    display: inline-block;
+    position: fixed;
+    top: 50%;
+    left: 85%;
+    opacity: 0.7;
 }
 .dot3 {
-  height: 50px;
-  width: 50px;
-  background-color: #34daff;
-  border-radius: 50%;
-  display: inline-block;
-  position: fixed;
-  top: 78%;
-  left: 81%;
+    height: 50px;
+    width: 50px;
+    background-color: #34daff;
+    border-radius: 50%;
+    display: inline-block;
+    position: fixed;
+    top: 78%;
+    left: 81%;
+}
+@media all and (min-width: 1324px) {
+    .dot4 {
+        transform: rotate(20deg);
+        height: 300px;
+        width: 660px;
+        background: linear-gradient(to right, #ee71b7, #d176d3);
+        border-radius: 0 0 0 10px;
+        display: inline-block;
+        position: fixed;
+        top: -10%;
+        left: 69%!important;
+        opacity: 0.9;
+    }
 }
 .dot4 {
-  transform: rotate(20deg);
-  height: 300px;
-  width: 660px;
-  background: linear-gradient(to right, #ee71b7, #d176d3);
-  border-radius: 0 0 0 10px;
-  display: inline-block;
-  position: fixed;
-  top: -10%;
-  left: 60%;
-  opacity: 0.9;
+    transform: rotate(20deg);
+    height: 300px;
+    width: 660px;
+    background: linear-gradient(to right, #ee71b7, #d176d3);
+    border-radius: 0 0 0 10px;
+    display: inline-block;
+    position: fixed;
+    top: -10%;
+    left: 60%;
+    opacity: 0.9;
 }
 .dot5 {
-  transform: rotate(-10deg);
-  height: 500px;
-  width: 600px;
-  background-color: #2cad31;
-  border-radius: 0 10px 0 0;
-  display: inline-block;
-  position: fixed;
-  top: 70%;
-  left: -20%;
-  opacity: 0.9;
+    transform: rotate(-10deg);
+    height: 500px;
+    width: 600px;
+    background: linear-gradient(-45deg, #00F260, #0575E6);
+    border-radius: 0 10px 0 0;
+    display: inline-block;
+    position: fixed;
+    top: 70%;
+    left: -20%;
+    opacity: 0.9;
 }
 .dot6 {
-  background: linear-gradient(45deg, #1dc8cd 0%, #55fabe 100%);
-  width: 300px;
-  height: 300px;
-  border-radius: 50%;
-  position: fixed;
-  top: -10%;
-  left: -10%;
+    background: linear-gradient(45deg, #1dc8cd 0%, #55fabe 100%);
+    width: 300px;
+    height: 300px;
+    border-radius: 50%;
+    position: fixed;
+    top: -10%;
+    left: -10%;
 }
 </style>
