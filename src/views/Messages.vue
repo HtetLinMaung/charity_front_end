245f05106b1ba9d94d3a73b494f2ad60136bd8cc
waiyanyenaing <wai.yan.ye.naing@fusionsol.com>
2019-09-05 14:17:16 +0630

    update

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -1,85 +1,20 @@
 <template>
   <div class="messages">
 
-      <v-col class="card_message" align="end" justify="end">
-        <v-card max-width="344" class="ml-auto mt-auto">
-          <v-card-title @click="show_message_toggle = !show_message_toggle">
-            <v-icon>mdi-message</v-icon>&nbsp;<b>Chat Mal </b><i>(Wai Yan)</i>
-          </v-card-title>
-          <v-card-text v-show="show_message_toggle">
-              <p :class="{ received: isReceived }"><span>hola</span></p>
-              <p :class="{ send: isReceived }"><span>hello</span></p>
-              <p :class="{ send: isReceived }"><span>hello</span></p>
-              <p :class="{ received: isReceived }"><span>hey</span></p>
-              <p :class="{ received: isReceived }"><span>hi</span></p>
-              <p :class="{ send: isReceived }"><span>ho</span></p>
-              <p :class="{ received: isReceived }"><span>text1</span></p>
-              <p :class="{ received: isReceived }"><span>text1</span></p>
-              <p :class="{ send: isReceived }"><span>text1</span></p>
-              <p :class="{ send: isReceived }"><span>text1</span></p>
-              <p :class="{ received: isReceived }"><span>text1</span></p>
-              <p :class="{ received: isReceived }"><span>text1</span></p>
-          </v-card-text>
-          <v-text-field v-show="show_message_toggle" filled append-icon="mdi-emoticon-poop" label="message pote mal"></v-text-field>
-        </v-card>
-      </v-col>
-
+      <Chatting />
 
   </div>
 </template>
 
 <script>
+import Chatting from '../components/Chat/Chatting.vue';
 export default {
-  data: () => ({
-    isReceived: true,
-    show_message_toggle: true,
-  }),
+  components: {
+    Chatting,
+  },
 };
 </script>
+
 <style>
-.card_message {
-  position: fixed;
-  bottom: 0;
-  right: 0;
-  width: auto;
-}
-.card_message .v-card {
-  min-width: 330px;
-  margin-right: 30rem;
-  border-radius: 18px 18px 0px 0px ;
-}
-.card_message .v-card__title {
-  background-color: rgb(226, 219, 219);
-  font-size: 1rem;
-  height: 3rem;
-  line-height: 0rem;
-  padding: 10px 16px 9px;
-}
-.card_message .theme--light.v-card > .v-card__text {
-  height: 250px;
-  overflow-y: scroll;
-}
-.m-textarea {
-  width: 15px;
-}
-.card_message .v-input__slot {
-  margin-bottom: 0px;
-}
-.card_message .v-input__slot{
-  margin-bottom: -21px;
-}
-.card_message .received span{
-  text-align: right;
- background-color: darkgray;
- padding: 7px 20px;
- border-radius: 10px 1px;
-}
-.card_message .send{
-  text-align: left;
-}
-.card_message span{
- background-color: aquamarine;
- padding: 7px 20px;
- border-radius: 10px 1px;
-}
+
 </style>
--- src/views/Sidebar.vue
+++ src/views/Sidebar.vue
@@ -1,34 +1,96 @@
 <template>
   <div class="sidebar">
-      <v-spacer></v-spacer>
-      <v-col class="card_sidebar md-1" align="end" justify="end">
-        <v-card max-width="344" class="">
-                <v-card-title><h5>Stories</h5></v-card-title>
-                <v-card-text>
-                    <ul>
-                        <li><img src="" alt=""><p>adwdwada</p></li>
-                        <li><img src="" alt=""><p>adwdwada</p></li>
-                        <li><img src="" alt=""><p>adwdwada</p></li>
-                        <li><img src="" alt=""><p>adwdwada</p></li>
-                        <li><img src="" alt=""><p>adwdwada</p></li>
-                        <li><img src="" alt=""><p>adwdwada</p></li>
-                        <li><img src="" alt=""><p>adwdwada</p></li>
-                        <li><img src="" alt=""><p>adwdwada</p></li>
-                        <li><img src="" alt=""><p>adwdwada</p></li>
-                    </ul>
-                </v-card-text>
-                <v-card-actions>
-                <v-btn text>Click</v-btn>
-                </v-card-actions>
-            </v-card>
+    <v-layout row wrap>
+      <v-col class="card_sidebar md-1" justify="end">
+        <v-card max-width="300" tile>
+          <v-list
+            :two-line="two-line"
+            :shaped="shaped"
+            :flat="flat"
+            :inactive="inactive"
+            :nav="nav"
+            :avatar="avatar"
+          >
+            <v-subheader>STORIES</v-subheader>
+            <v-list-item-group v-model="item" color="primary">
+              <v-list-item v-for="(item, i) in items" :key="i">
+                <v-list-item-avatar v-if="avatar">
+                  <v-img :src="item.avatar"></v-img>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title v-html="item.title"></v-list-item-title>
+                  <v-list-item-subtitle v-if="twoLine || threeLine" v-html="item.subtitle"></v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+        <br>
+        <v-card max-width="300" tile>
+          <v-list
+            :two-line="two-line"
+            :shaped="shaped"
+            :flat="flat"
+            :inactive="inactive"
+            :nav="nav"
+            :avatar="avatar"
+          >
+            <v-subheader>IMPORTANT A HLU KHAN</v-subheader>
+            <v-list-item-group v-model="item" color="primary">
+              <v-list-item v-for="(item, i) in items" :key="i">
+                <v-list-item-avatar v-if="avatar">
+                  <v-img :src="item.avatar"></v-img>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title v-html="item.title"></v-list-item-title>
+                  <v-list-item-subtitle v-if="twoLine || threeLine" v-html="item.subtitle"></v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
       </v-col>
+    </v-layout>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-
+    item: 5,
+    twoLine: true,
+    shaped: true,
+    flat: true,
+    inactive: true,
+    nav: true,
+    avatar: true,
+    items: [
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+        title: 'Brunch this weekend?',
+        subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+      },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+        subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+      },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+        title: 'Oui oui',
+        subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
+      },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+        title: 'Birthday gift',
+        subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
+      },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+        title: 'Recipe to try',
+        subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      },
+    ],
   }),
 };
 </script>
@@ -37,8 +99,25 @@ export default {
   position: fixed;
   top: 0;
   margin-top: 70px;
+  right: 0;
+  width: auto;
+}
+.card_sidebar .v-card{
+  margin-right: 5rem;
+  border-radius: 15px 15px 5px 5px ;
+}
+.card_sidebar .primary--text {
+    height: 270px;
+    overflow-y: scroll;
 }
 .card_sidebar ul{
     list-style-type: none;
+}
+.card_contacts_sidebar{
+  position: fixed;
+  top: 0;
+  margin-top: 70px;
+  right: 0;
+  width: auto;
 }
 </style>
