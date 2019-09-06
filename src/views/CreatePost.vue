806f0c187cb577a35e66b263c04b3287d8690edf
Kyi Sin Thant <kyi.sin.thant@fusionsol.com>
2019-09-06 15:23:18 +0630

    edit

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -16,7 +16,7 @@
         <v-avatar class="mt-3 ml-5 mb-3" size="40">
           <v-img :src="images.profile"></v-img>
         </v-avatar>
-        <v-card-title class="text">What's on your mind?</v-card-title>
+        <v-card-title class="text mb-3" >What's on your mind?</v-card-title>
         <div class="flex-grow-1"></div>
         <v-btn icon class="mt-5 mb-3 mr-3" color="black">
           <v-icon>mdi-image-filter</v-icon>
